74868fcc3b393eed0e3fbfe36ecff0848d9fee23
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-10-19 11:38:20 +0300

    SignUp(Üye ol) Sayfasının tasarlanması

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -23,6 +23,20 @@ namespace AspNetCoreIdentityApp.Web.Controllers
             return View();
         }
 
+
+        public IActionResult SignUp()
+        {
+
+
+            return View();
+        }
+
+
+
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+
+        public SignUpViewModel(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+
+        [Display(Name ="Kullanıcı Adı :")]
+        public string UserName { get; set; }
+        [Display(Name = "Email :")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Telefon :")]
+        public string Phone { get; set; }
+
+        [Display(Name =  "Şifre :")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre Tekrar :")]
+        public string PasswordConfirm { get; set; }
+    }
+}
